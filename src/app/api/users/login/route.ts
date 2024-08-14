594bf2e8cb9ece6b3c6cf4f28e5cafeaa81c9350
punyeeet <puneetpradhan007@gmail.com>
2024-08-14 07:40:15 +0530

    added logs to debug login file

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,61 +1,70 @@
 import { connect } from "@/app/dbConfig/dbConfig";
-import User from "@/models/userModel"
+import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
-import bcryptjs from "bcryptjs"
-import jwt from "jsonwebtoken"
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-
-connect()
+connect();
 
 export async function POST(request: NextRequest) {
-
     try {
-
         const reqBody = await request.json();
+        console.log("Request Body:", reqBody);
+
         const { email, password } = reqBody.data;
 
         // checking if user exists
         const user = await User.findOne({ email });
-
         if (!user) {
-            return NextResponse.json({
-                error: 'User Does not exist'
-            }, { status: 400 })
+            return NextResponse.json(
+                {
+                    error: "User does not exist",
+                },
+                { status: 400 }
+            );
         }
 
-
-        // check if password exits
-        const validPassword = await bcryptjs.compare(password,user.password)
-
-        if(!validPassword){
-            return NextResponse.json({error: "Invalid Password"}, { status: 400})
+        // check if password is correct
+        const validPassword = await bcryptjs.compare(password, user.password);
+        if (!validPassword) {
+            return NextResponse.json(
+                { error: "Invalid Password" },
+                { status: 400 }
+            );
         }
 
         // create token data
         const tokenData = {
             id: user._id,
             username: user.username,
-            email: user.email
+            email: user.email,
+        };
+
+        if (!process.env.TOKEN_SECRET) {
+            throw new Error("Missing TOKEN_SECRET environment variable");
         }
 
-        const token = jwt.sign(tokenData, process.env.TOKEN_SECRET! , { expiresIn: "1d"})
+        const token = jwt.sign(tokenData, process.env.TOKEN_SECRET, {
+            expiresIn: "1d",
+        });
 
         const response = NextResponse.json({
             message: "Login successful",
             success: true,
-        })
+        });
 
-        response.cookies.set("token",token, {
+        response.cookies.set("token", token, {
             httpOnly: true,
-        })
+        });
 
         return response;
-
     } catch (error: any) {
-        return NextResponse.json({
-            error: error.message
-        }, { status: 500 })
+        console.error("Error:", error);
+        return NextResponse.json(
+            {
+                error: error.message,
+            },
+            { status: 500 }
+        );
     }
-
-
 }
